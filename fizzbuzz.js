69e85af9f792c7652bbba185b7f672a9f0d03f2b
Reed Nelson-Saunders <reedns@hotmail.com>
2014-09-30 12:10:31 -0700

    initial commmit

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+var fizzBuzz = function (start, end) {
+    for (var i = start; i < end; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+};
+
+fizzBuzz(1, 1000)
